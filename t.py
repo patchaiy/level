c4605426f65818d563190574b046cce47c276c05
patchaiy <50619625+patchaiy@users.noreply.github.com>
2019-07-22 19:52:24 +0530

    Create t.py

---
+++ t.py
@@ -0,0 +1,16 @@
+Ai=int(input())
+b=str(Ai)
+k=[]
+s=[]
+summ=0
+for i in range(0,len(b)):
+    s.append(b[i])
+k=list(map(int,s))
+if len(k)==1:
+    print(k[0]**2)
+else:
+    for i in range(0,len(k)-1):
+        summ=summ+(k[i]**k[i+1])
+    else:
+        summ=summ+(k[-1]**k[0])
+    print(summ)
