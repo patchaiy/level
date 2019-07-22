efcc2f44f2765f65d9f76eb92ef45bf64217b416
patchaiy <50619625+patchaiy@users.noreply.github.com>
2019-07-22 19:27:55 +0530

    Update h.py

--- h.py
+++ h.py
@@ -1,14 +1,21 @@
-ti=int(input())
-s=[]
-x=0
-for i in range(2,t):
-    for j in range(2,i):
-        if i%j==0:
-            break
-    else:
-        s.append(i)
-for m in range(0,len(s)):
-    for n in range(m,len(s)):
-        if s[m]+s[n]==ti:
-            x=x+1
-print(x)
+end = int(input())
+sta = 1
+sprime = [True for i in range(end+1)] 
+p = 2
+out = []
+outlist = []
+code = 0
+count = 0
+while (p * p <= end): 
+    if (sprime[p] == True): 
+        for i in range(p * p, end+1, p): 
+            sprime[i] = False
+    p += 1
+for p in range(sta, end): 
+        if sprime[p] and p!=1: 
+            outlist.append(p)
+for i in range(len(outlist)):
+    for j in range(i,len(outlist)):
+        if (outlist[i] + outlist[j]) == end:
+            count += 1
+print(count)
