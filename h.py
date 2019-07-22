30319845b79613e9a5c53890ff1655e839f46fd4
patchaiy <50619625+patchaiy@users.noreply.github.com>
2019-07-22 19:30:21 +0530

    Update h.py

--- h.py
+++ h.py
@@ -1,21 +1,21 @@
-end = int(input())
+endi = int(input())
 sta = 1
-sprime = [True for i in range(end+1)] 
+sprime = [True for i in range(endi+1)] 
 p = 2
 out = []
 outlist = []
 code = 0
 count = 0
-while (p * p <= end): 
+while (p * p <= endi): 
     if (sprime[p] == True): 
-        for i in range(p * p, end+1, p): 
+        for i in range(p * p, endi+1, p): 
             sprime[i] = False
     p += 1
-for p in range(sta, end): 
+for p in range(sta, endi): 
         if sprime[p] and p!=1: 
             outlist.append(p)
 for i in range(len(outlist)):
     for j in range(i,len(outlist)):
-        if (outlist[i] + outlist[j]) == end:
+        if (outlist[i] + outlist[j]) == endi:
             count += 1
 print(count)
